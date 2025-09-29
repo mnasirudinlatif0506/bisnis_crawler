621f9789eedc2cea599456aa9df32d84872ff679
mnasirudinlatif0506 <mnasirudinlatif0506@gmail.com>
2025-09-29 14:26:29 +0700

    change-headers

--- crawler/core.py
+++ crawler/core.py
@@ -14,7 +14,7 @@ logger = logging.getLogger("bisnis-crawler")
 
 # header request
 DEFAULT_HEADERS = {
-    "User-Agent": "BisnisCrawler/1.0"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36"
 }
 
 # timezone jakarta
